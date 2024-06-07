7dc7e9a99fb551b3ca2f8b599ba185b3f26fb878
Sidratul Montaha <sonykhan1121@gmail.com>
2024-06-07 18:59:41 +0600

    check valid floating number

--- Detect_Floating_Point_Number.py
+++ Detect_Floating_Point_Number.py
@@ -2,6 +2,8 @@ tc = int(input())
 for i in range(tc):
     s = input()
     try:
+        if '.' not in s:
+            s+='a'
         now = float(s)
     except ValueError :
         print("False")
